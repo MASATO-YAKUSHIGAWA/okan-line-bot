f926c41ca6c6a2777f59a9ab170247928b316a96
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-13 17:38:08 +0900

    現在地情報取得

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -8,22 +8,34 @@ class LinebotController < ApplicationController
   protect_from_forgery :except => [:callback]
 
   def callback
-
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
 
     events = client.parse_events_from(body)
     events.each { |event|
 
     case event
+    when Line::Bot::Event::Follow
+      client.reply_message(event['replyToken'], first_message)
+
+    when Line::Bot::Event::Unfollow # LINEお友達解除された場合
+      line_id = event['source']['userId'] # お友達解除したユーザーのデータをユーザーテーブルから削除
+      User.find_by(line_id: line_id).destroy
+    
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
+        line_id = event['source']['userId'] #line_id
+        user = User.find_by(line_id: line_id) #linee_id取得
+        user_location = AreaInfo.find(user.area_info_id) #userの観測値情報取得
         input = event.message['text']
-        url  = "https://www.drk7.jp/weather/xml/27.xml"
+        url  = "https://www.drk7.jp/weather/xml/#{user_location.prep_id}.xml"
         xml  = open( url ).read.toutf8
         doc = REXML::Document.new(xml)
-        xpath = 'weatherforecast/pref/area[1]/'
+        xpath = "weatherforecast/pref/area[#{user_location.area_id}]/"
 
         min_per = 30
         case input
@@ -35,70 +47,72 @@ class LinebotController < ApplicationController
           per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
           if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
             push =
-              "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+              "明日の#{user_location.prep_name}、#{user_location.area_name}の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
           else
             push =
-              "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+              "明日の天気？\n明日の#{user_location.prep_name}、#{user_location.area_name}は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
           end
         end
 
       when Line::Bot::Event::MessageType::Location # 位置情報が入力された場合
-        latitude = event.message['latitude'] # 緯度
-        longitude = event.message['longitude'] # 経度
-
-
+        lat = event.message['latitude'] # 緯度
+        long = event.message['longitude'] # 経度
+        line_id = event['source']['userId'] #line_id
+        push = "ありがとう"
+        area = AreaInfo.find_by_sql(["select * from area_infos order by abs(latitude - ?) + abs(longitude - ?) ASC limit 1 ", lat, long]) #現在地から一番近い観測地点を取得
+        if User.find_by(line_id: line_id)
+          User.update(line_id: line_id, area_info_id: area.first.id) #ユーザー情報を更新
+        else
+          User.create(line_id: line_id, area_info_id: area.first.id) #ユーザー情報を保存
+        end
       end
   
-    when Line::Bot::Event::Follow
-      push = "こんにちは！\n位置情報を送ってね！"
-      client.reply_message(event['replyToken'], location)
-
-        #   # 登録したユーザーのidをユーザーテーブルに格納
-        #   line_id = event['source']['userId']
-        #   User.create(line_id: line_id)
-        #   # LINEお友達解除された場合（機能③）
-        # when Line::Bot::Event::Unfollow
-        #   # お友達解除したユーザーのデータをユーザーテーブルから削除
-        #   line_id = event['source']['userId']
-        #   User.find_by(line_id: line_id).destroy
-        # end
-    end
-    message = {
-      "type": 'text',
-      "text": push
-    }
-    client.reply_message(event['replyToken'], message)
-
-    }
-    head :ok
-  end
-  
-  def location
-    {
-      "type": "template", #テンプレートメッセージオブジェクトの共通プロパティ
-      "altText": "位置検索中",
-      "template": {          #テンプレート指定
-          "type": "buttons", #ボタンテンプレート使用
-          "title": "現在位置検索",
-          "text": "現在の位置を送信しますか？",
-          "actions": [
-              {
-                "type": "uri",
-                "label": "現在位置を送る",
-                "uri": "line://nv/location" #位置情報画を開くスキーム
-              }
-          ]
+      message = {
+        "type": 'text',
+        "text": push
       }
-    }
+      client.reply_message(event['replyToken'], message)
+
+
+    end
+  }
+  head :ok
+end
+  
+  def first_message
+    [
+      {"type": 'text',
+      "text": "こんにちは！\n現在位置を送ってね！"},
+      {"type": "template", #テンプレートメッセージオブジェクトの共通プロパティ
+        "altText": "位置検索中",
+        "template": {          #テンプレート指定
+            "type": "buttons", #ボタンテンプレート使用
+            "title": "現在位置検索",
+            "text": "現在の位置を送信しますか？",
+            "actions": [
+                {
+                  "type": "uri",
+                  "label": "現在位置を送る",
+                  "uri": "line://nv/location" #位置情報画を開くスキーム
+                }
+            ]
+        }
+      }
+    ]
   end
 
   private
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET_ID"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      # ローカル環境
+      config.channel_secret = "f25a9c6c930126ad7d1f291e3771b4a8"
+      config.channel_token = "8BvTmnSr8fP37Hq3EEhaxtQLPpngKgv4tlK/Mpml90NsR9N4UjVx5Iudtg07AQ16NqTsuITHGmdXQ/PsaHqDiB1pHPA6ivNLOozl2MIdJqQzf3PbnV3C+5m+kVpTt2cx/YaYXAj9tRH2+GNgX6R9hgdB04t89/1O/w1cDnyilFU="
     }
+      # 本番環境
+    #   config.channel_secret = ENV["LINE_CHANNEL_SECRET_ID"]
+    #   config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    # }
   end
 
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -11,25 +11,31 @@ task :update_feed => :environment do
   }
 
   # 使用したxmlデータ（毎日朝6時更新）：以下URLを入力すれば見ることができます。
-  url  = "https://www.drk7.jp/weather/xml/27.xml"
+  line_id = event['source']['userId'] #line_id
+  user = User.find_by(line_id: line_id) #linee_id取得
+  user_location = AreaInfo.find(user.area_info_id) #userの観測値情報取得
+  url  = "https://www.drk7.jp/weather/xml/#{user_location.prep_id}.xml"
   # xmlデータをパース（利用しやすいように整形）
   xml  = open( url ).read.toutf8
   doc = REXML::Document.new(xml)
   # パスの共通部分を変数化（area[1]は「大阪地方」を指定している）
-  xpath = 'weatherforecast/pref/area[1]/info/rainfallchance/'
+  xpath = "weatherforecast/pref/area[#{user_location.area_id}]/info/rainfallchance/"
   # 6時〜12時の降水確率（以下同様）
   per06to12 = doc.elements[xpath + 'period[2]'].text
   per12to18 = doc.elements[xpath + 'period[3]'].text
   per18to24 = doc.elements[xpath + 'period[4]'].text
 
    # 降水確率によってメッセージを変更する閾値の設定
-   mid_per = 50
-   if per06to12.to_i >= mid_per || per12to18.to_i >= mid_per || per18to24.to_i >= mid_per
-     word3 = "今日は雨が降りそうだから傘を忘れないでね！"
-     word3 = "今日は雨が降るかもしれないから折りたたみ傘があると安心だよ！"
+  mid_per = 50
+  if per06to12.to_i >= mid_per || per12to18.to_i >= mid_per || per18to24.to_i >= mid_per
+    word3 = "今日は雨が降りそうだから傘を忘れないでね！"
+    word3 = "今日は雨が降るかもしれないから折りたたみ傘があると安心だよ！"
    # 発信するメッセージの設定
-   push =
+    push =
      "#{word3}\n降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％"
+  else
+    push = "こんにちは"
+    
    # メッセージの発信先idを配列で渡す必要があるため、userテーブルよりpluck関数を使ってidを配列で取得
    user_ids = User.all.pluck(:line_id)
    message = {
