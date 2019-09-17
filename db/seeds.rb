32b084643e2490d13d0146ebe6d77dcd33b7d3b6
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-17 15:20:28 +0900

    fix seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 AreaInfo.create!(
   [
     {
-    prep_name: "大阪府"
-    prep_id: "27"
-    area_name: "大阪地方"
-    area_id: "1"
-    latitude: "34.7629"
-    longitude: "135.5681"
+    prep_name: "大阪府",
+    prep_id: "27",
+    area_name: "大阪地方",
+    area_id: "1",
+    latitude: "34.7629",
+    longitude: "135.5681",
     },
     {
-    prep_name: "東京都"
-    prep_id: "13"
-    area_name: "東京地方"
-    area_id: "4"
-    latitude: "35.6862"
-    longitude: "139.7498"
-    },
+    prep_name: "東京都",
+    prep_id: "13",
+    area_name: "東京地方",
+    area_id: "4",
+    latitude: "35.6862",
+    longitude: "139.7498",
+    }
   ]
 )
