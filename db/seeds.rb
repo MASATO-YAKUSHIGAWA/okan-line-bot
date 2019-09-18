34348365751246527dac02ec4c16160e0bc8d83b
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-18 13:33:02 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,6 @@
-AreaInfo.create!(
-  [
-    {
-    prep_name: "大阪府",
-    prep_id: "27",
-    area_name: "大阪地方",
-    area_id: "1",
-    latitude: "34.7629",
-    longitude: "135.5681",
-    },
-    {
-    prep_name: "東京都",
-    prep_id: "13",
-    area_name: "東京地方",
-    area_id: "4",
-    latitude: "35.6862",
-    longitude: "139.7498",
-    }
-  ]
-)
+require 'csv'
+
+csv_data = CSV.read('db/jp_area_infos.csv', headers: true)
+csv_data.each do |data|
+  AreaInfo.create!(data.to_hash)
+end
