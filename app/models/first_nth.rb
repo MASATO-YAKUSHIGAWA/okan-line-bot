5a04db7c20230128e470d6a994d06e1aded38bef
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-19 10:08:34 +0900

    fix model(active_hash)

--- app/models/first_nth.rb
+++ app/models/first_nth.rb
@@ -1,9 +1,9 @@
 class FirstNth < ActiveHash::Base
   self.data = [
-    {id: 1, name: '第一'},
-    {id: 2, name: '第二'},
-    {id: 3, name: '第三'},
-    {id: 4, name: '第四'},
+    {id: 1, name: '第一週'},
+    {id: 2, name: '第二週'},
+    {id: 3, name: '第三週'},
+    {id: 4, name: '第四週'},
     {id: 5, name: '毎週'},
   ]
 end
