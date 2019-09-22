0b15ae55210c3c7d38d18bebaeb2a356cce09892
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-22 13:10:34 +0900

    fix seed file

--- db/fixtures/seeds.rb
+++ db/fixtures/seeds.rb
@@ -2,5 +2,12 @@ require 'csv'
 
 csv_data = CSV.read('db/fixtures/jp_area_infos.csv', headers: true)
 csv_data.each do |data|
-  AreaInfo.create!(data.to_hash)
+  AreaInfo.seed do |s|
+    s.prep_name = data[1]
+    s.prep_id = data[2]
+    s.area_name = data[3]
+    s.area_id = data[4]
+    s.latitude = data[5]
+    s.longitude = data[6]
+  end
 end
