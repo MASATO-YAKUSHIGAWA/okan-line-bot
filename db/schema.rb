92c2d834643240b57ea641656c3ebcbae9084e86
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-09-22 12:37:05 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_051043) do
+ActiveRecord::Schema.define(version: 2019_09_14_082436) do
 
   create_table "area_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prep_name"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 2019_09_18_051043) do
   create_table "garbages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "wday_id"
     t.string "first_nth_id"
+    t.string "second_nth_id"
     t.string "garbage_type_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "second_nth_id"
     t.index ["user_id"], name: "index_garbages_on_user_id"
   end
 
